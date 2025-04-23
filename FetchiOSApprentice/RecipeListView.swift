3d5006764c08e00b4e06b49d5839765d987c82f8
Tim McEwan <timmcewandev@gmail.com>
2025-04-23 13:43:49 -0600

    Removed initial view and made and replaced it with RecipeListView

---
+++ FetchiOSApprentice/RecipeListView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeListView.swift
+//  FetchiOSApprentice
+//
+//  Created by Tim McEwan on 4/23/25.
+//
+
+import SwiftUI
+
+struct RecipeListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeListView()
+}
