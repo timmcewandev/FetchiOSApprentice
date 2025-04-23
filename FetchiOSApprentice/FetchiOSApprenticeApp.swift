be8a77ea5ae5b15608e99fd001ee02df1c502038
Tim McEwan <timmcewandev@gmail.com>
2025-04-23 13:38:36 -0600

    Initial Commit

---
+++ FetchiOSApprentice/FetchiOSApprenticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchiOSApprenticeApp.swift
+//  FetchiOSApprentice
+//
+//  Created by Tim McEwan on 4/23/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchiOSApprenticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
